c30f80f55951ca106c9320c60a8811a8fab35ed4
escc1122 <escc1122@gmail.com>
2017-03-23 09:35:43 +0800

    Update plugin_redirect.js

--- suitescript2.0/suitelet/plugin_redirect.js
+++ suitescript2.0/suitelet/plugin_redirect.js
@@ -1,12 +1,16 @@
 /**
  * @NApiVersion 2.0
  * @NScriptType suitelet
+ ＊ plugin路徑 //netsuite/suitescript2.0/plugin/plugin.js
+ * 使用 plugin & redirect Modules 利用plugin讀取資料並進行轉頁
+ ＊
  */
 define(['N/plugin','N/redirect'], function(plugin,redirect) {
 	var tmp = {
         onRequest: function(options) {
                 options.response.write('aaaaaaaaaaaaaa'+ '\n');
       
+		//netsuite/suitescript2.0/plugin/plugin.js
                     var pl = plugin.loadImplementation({
                         type: 'customscript433'//plugin的id
                     });
