6cab67fa91aec9c5a318a70d39491c6fee73bc85
escc1122 <escc1122@gmail.com>
2017-03-24 13:41:50 +0800

    Create al_serach.js

---
+++ suitescript2.0/suitelet/al_serach.js
@@ -0,0 +1,43 @@
+/**
+ *@NApiVersion 2.x
+ *@NScriptType suitelet
+ */
+ define(['N/record','N/search'],function(record,search) {
+	 var tmp ={
+	 onRequest: function(options) {
+     	options.response.write(search.Operator.IS+"<br>");
+     	options.response.write(search.Type.EMPLOYEE+"<br>");
+     	
+     	var mySalesOrderSearch = search.create({
+     	    type: search.Type.EMPLOYEE, // or customer record id
+     	    title: 'al_search',
+     	    columns: ['email','altname'],
+     	    filters: [{
+     	        name: 'email',
+     	        operator: search.Operator.IS,
+     	        values: ['escc1122@gmail.com']
+     	    }]
+     	});
+     
+     	
+     	var resultSet=mySalesOrderSearch.run();
+ 		
+     	resultSet.each(function(result) {
+     		
+            var name = result.getValue({
+                name: 'altname'
+            });
+            var email = result.getValue({
+                name: 'email'
+            });
+
+            options.response.write(name+"=====>"+email+"<br>");
+        });
+     	
+     	
+
+	 	}
+	 }
+	 return tmp;
+    
+    });
