fc3137f2cc99e0c54b92eaef058ff0278916626c
escc1122 <escc1122@gmail.com>
2017-04-18 11:47:51 +0800

    Create record2.js

---
+++ suitescript2.0/Modules/record/record2.js
@@ -0,0 +1,82 @@
+/**
+ * @NApiVersion 2.0
+ * @NScriptType suitelet
+ */
+define(['N/record'], function(record) {
+	var tmp = {
+        onRequest: function(options) {
+        	var parameters = options.request.parameters;
+        	
+        	var id = parameters.id;
+        	var recordType = parameters.recordType;
+                
+//        	var type = 'customrecord_aaaaa_bbbb';
+//        	var type = record.Type.SALES_ORDER;
+//        	var type = record.Type.CUSTOMER;
+        	var type = eval('record.Type.'+recordType);
+        	var objRecord = record.load({
+        	    type: type, 
+        	    id: id
+        	});
+        	var array = objRecord.getFields();
+        	for (var i=0;i<array.length;i++){
+        		var txt = objRecord.getText({
+        			fieldId: array[i]
+        		});
+        		var value = objRecord.getValue({
+        			fieldId: array[i]
+        		});
+//        		var objField = objRecord.getField({
+//        		    fieldId: array[i]
+//        		});
+        		options.response.write(array[i]+"==type:"+typeof(value)+"===>"+txt+"====>"+value+"<br>");
+        	}
+
+			options.response.write("=======================================sublist=============================================<br>");
+			options.response.write("=======================================sublist=============================================<br>");
+    		//取得sublist name
+    		var sublistNames = objRecord.getSublists();
+    		for (var i=0;i<sublistNames.length;i++){
+    			
+    			var sublistName = sublistNames[i];
+    			options.response.write("==============================sublistName============================================"+sublistName+"<br>");
+    			
+    			//取得sublistLineCount
+        		var numLines = objRecord.getLineCount({
+        		    sublistId: sublistName
+        		});
+        		
+        		var SublistSubrecord = objRecord.getSublistFields({
+        		    sublistId: sublistName
+        		});
+        		
+        		for (var k=0;k<numLines;k++){
+        			options.response.write("==============================numLines============================================"+k+"<br>");
+            		for (var j=0;j<SublistSubrecord.length;j++){
+                		var getSublistValue = objRecord.getSublistValue({
+                		    sublistId: sublistName,
+                		    fieldId: SublistSubrecord[j],
+                		    line: k  
+                		});
+                		
+                		var getSublistText = objRecord.getSublistText({
+                		    sublistId: sublistName,
+                		    fieldId: SublistSubrecord[j],
+                		    line: k  
+                		});
+            			
+            			
+            			options.response.write(SublistSubrecord[j]+"======type : "+typeof(getSublistValue)+"========Value : "+getSublistValue+"===========Text : "+getSublistText+"<br>");
+            		}
+        		}
+    		}
+    		
+
+    		   	
+        	
+        	
+        }
+    };
+	
+    return tmp;
+});
