9067bd948207ff5063b6525dbdb0514ccd2d05c5
escc1122 <escc1122@gmail.com>
2017-09-05 13:36:28 +0800

    Create record_create_deposit.js

---
+++ suitescript2.0/Modules/record/record_create_deposit.js
@@ -0,0 +1,54 @@
+/**
+ * @NApiVersion 2.x
+ * @NScriptType Suitelet
+ * @NModuleScope SameAccount
+ */
+define(['N/record'],
+function(record) {
+    function onRequest(context) {
+    	var type = record.Type.DEPOSIT;
+    	var isDynamic=false;
+    	var objRecord = record.create({
+    	    type: type,
+    	    isDynamic:isDynamic
+    	});
+        objRecord.setValue({
+            fieldId: 'account',
+            value: '1800'
+        });
+    	
+         objRecord.setValue({
+              fieldId: 'subsidiary',
+              value: '1'
+          });
+         
+         objRecord.setText({
+             fieldId: 'customform',
+             value: 'aaaaaaaa'
+         });
+           		
+    	objRecord.setSublistValue({
+    	    sublistId: 'payment',
+    	    fieldId: 'id',
+    	    line: 1,
+    	    value: "1610"
+    	});
+    	
+    	objRecord.setSublistValue({
+    	    sublistId: 'payment',
+    	    fieldId: 'deposit',
+    	    line: 1,
+    	    value: true
+    	});
+    	    	   	
+    	var id = objRecord.save();
+    	
+    	context.response.write("====id==>"+id)
+    	
+    }
+
+    return {
+        onRequest: onRequest
+    };
+    
+});
